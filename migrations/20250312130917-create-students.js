dd2445330afb5a25b0de6c77cc5f6e155744370f
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-12 16:14:41 +0300

    fix: edited students table code

--- migrations/20250312130917-create-students.js
+++ migrations/20250312130917-create-students.js
@@ -1,22 +1,62 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
+
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+  await queryInterface.createTable("Students", {
+  id: {
+  type: Sequelize. INTEGER,
+  autoIncrement: true,
+  primaryKey: true,
+  allowNull: false
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  name: {
+  type: Sequelize.STRING,
+  allowNull: false
+  },
+
+  
+  email: {
+  type: Sequelize.STRING,
+  allowNull: false,
+  unique: true
+  },
+
+  createdAt: {
+  type: Sequelize.DATE,
+  allowNull: false
+  },
+
+  updatedAt: {
+  type: Sequelize.DATE,
+  allowNull: false
   }
-};
+});
+
+},
+  down: async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable("Students");
+  }
+  };
