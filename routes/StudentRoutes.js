be97166173efd71691d3becb89633ea61f4c7a06
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-13 15:33:52 +0300

    feat:added StudentRoutes.js under routes folder😶‍🌫️

---
+++ routes/StudentRoutes.js
@@ -0,0 +1,37 @@
+
+
+const express = require("express");
+const router = express. Router();
+const { Student } = require(" .. /models");
+
+// Create a student
+router.post("/students", async (req, res) => {
+const student = await Student.create(req.body);
+res.status(201).json(student);
+});
+
+// Get all students
+router.get("/students", async (req, res) => {
+const students = await Student.findA11();
+res.json(students);
+});
+
+// Get student by ID
+router.get("/students/:id", async (req, res) => {
+const student = await Student.findByPk(req.params.id);
+res.json(student);
+});
+
+// Update student
+router.put("/students/:id", async (req, res) => {
+await Student.update(req.body, { where: { id: req.params.id } });
+res.json({ message: "Updated successfully" });
+});
+
+// Delete student
+router.delete("/students/:id", async (req, res) => {
+await Student.destroy({ where: { id: req.params.id } });
+res.status(204).send();
+});
+
+module.exports = router;
